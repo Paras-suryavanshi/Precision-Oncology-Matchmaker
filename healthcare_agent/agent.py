ebc40c2ee621adcd554603e232966f1880d6789a
Paras-suryavanshi <parassuryavanshi257@gmail.com>
2026-05-14 01:25:11 +0530

    fix bug

--- healthcare_agent/agent.py
+++ healthcare_agent/agent.py
@@ -76,7 +76,7 @@ def fetch_data_and_inject(callback_context, llm_request):
 # 2. Define the Agent
 root_agent = Agent(
     name="healthcare_fhir_agent",
-    model="gemini-1.5-flash-002",
+    model="gemini-3.1-flash-lite",
     description="Evaluates patient eligibility for clinical trials.",
     instruction=(
         """
